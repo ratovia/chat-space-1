f9651c8878d637e86cc3c4bea82f2c8bd6553bbb
kuu0123 <bms.01230214@gmail.com>
2019-10-08 16:25:34 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -21,8 +21,9 @@ $(function(){
   }
 
   var buildMessageHTML = function(message) {
-    if (message.content && message.image.url) {
-      //data-idが反映されるようにしている
+    var content = message.content ?  `<p class="lower-message__content">${message.content}</p>` : "";
+    var image = message.image.url ?  `<img class="lower-message__image" src="${message.image.url}">` : "";
+
       var html = `<div class="message" data-message-id="${message.id}">
         <div class="upper-message">
           <div class="upper-message__user-name">
@@ -34,44 +35,11 @@ $(function(){
         </div>
         <div class="lower-message">
           <p class="lower-message__content">
-            ${message.content}
+            ${content}
           </p>
-          <img src="${message.image.url}" class="lower-message__image" >
+          ${image}
         </div>
       </div>`
-    } else if (message.content) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-message-id="${message.id}">
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
-          </div>
-          <div class="upper-message__date">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-        </div>
-      </div>`
-    } else if (message.image.url) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-message-id="${message.id}">
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
-          </div>
-          <div class="upper-message__date">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="lower-message">
-          <img src=${message.image.url}" class="lower-message__image" >
-        </div>
-      </div>`
-    };
     return html;
   };
 
