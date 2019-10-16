5922f53ee105ba5ff7e33ff6eb7ff848275b87f2
kuu0123 <bms.01230214@gmail.com>
2019-10-16 10:15:38 +0900

    update incrementalSearch

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,11 +45,11 @@ namespace :deploy do
   after :finishing, 'deploy:cleanup'
 end
 
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
+# set :default_env, {
+#   rbenv_root: "/usr/local/rbenv",
+#   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+#   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+#   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+# }
 
 set :linked_files, %w{ config/secrets.yml }
