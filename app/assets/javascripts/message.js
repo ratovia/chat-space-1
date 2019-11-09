90d58dccc4e27e7ce078210ba2cd261dc6701118
kuu0123 <bms.01230214@gmail.com>
2019-11-09 14:22:30 +0900

    Minor update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -33,16 +33,20 @@ $(function(){
       processData: false,
       contentType: false
     })
+
     .done(function(data){
       var html = buildHTML(data);
       $('.main__chat').append(html);
       $('form')[0].reset();
-      $('.form__submit').attr('disabled', false);
       $('.main__chat').animate({ scrollTop: $('.main__chat')[0].scrollHeight});
     })
     .fail(function(){
-      alert('error');
+      alert('error');      
     })
+    .always(function() {
+      $('.form__submit').removeAttr('disabled');
+      // $('.form__submit').attr('disabled', false);
+    });
   })
 
   var reloadMessages = function() {
@@ -52,7 +56,7 @@ $(function(){
     if(location.href.match(groupid)){
     
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    last_message_id = $('.message').last().data('message-id');
+    message_id = $('.message').last().data('message-id');
     $.ajax({
       //ルーティングで設定した通りのURLを指定
       url: 'api/messages',
@@ -60,7 +64,7 @@ $(function(){
       type: 'get',
       dataType: 'json',
       //dataオプションでリクエストに値を含める
-      data: {id: last_message_id}
+      data: {message_id: message_id}
     })
     .done(function(messages) {
       //追加するHTMLの入れ物を作る
